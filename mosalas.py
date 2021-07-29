734b01d627e11c6e778f97bcf8a1b15f8dbff598
helia1385mir <88113615+helia1385mir@users.noreply.github.com>
2021-07-29 12:30:03 +0430

    Add files via upload

---
+++ mosalas.py
@@ -0,0 +1,7 @@
+x=int(input('adad aval zel mosalas ra vared konid'))
+y=int(input('adad dovom zel mosalas ra vared konid'))
+z=int(input('adad sevom zel mosalas ra vared konid'))
+if z<x+y and x<z+y and y<z+x :
+    print('mosalas sakhte shod')
+else :
+    print('mosalas tashkil n mishavad')
